9f582bb4216bbe08504ef964a1c7b375a26f7043
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-11 10:14:17 -0500

    Add + safe; other one was wrong ver.

--- WordSearch.java
+++ WordSearch.java
@@ -74,105 +74,156 @@ public WordSearch( int rows, int cols, String fileName, int randSeed)
      * or there are overlapping letters that do not match, then false is returned
      * and the board is NOT modified.
      */
-     public boolean checkH (String word, int row, int col){
-       boolean safe = true;
-       for (int i = 0; i < word.length(); col ++){
-       if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-         return false;
-       }
-       else {i ++;}
-     }
-     return safe;}
-
-    public boolean addWordHorizontal(String word,int row, int col){
-      if(row >= data.length || ((col + word.length() - 1) >= data[0].length)){
-        return false;
-      }
-      if (checkH(word, row, col) == false){
-        return false;
-      }
-
-      else { for (int i = 0; i < word.length(); col ++){
-               data[row][col] = word.charAt(i);
+private boolean addWord( int r, int c, String word, int rowIncrement, int colIncrement){
+if (Safe(r,c,word,rowIncrement,colIncrement) == false){return false;}
+  if (rowIncrement == 1){
+        if (colIncrement == 1){
+           for (int i = 0; i < word.length(); col ++){
+               data[r][c] = word.charAt(i);
                i ++;
-      }}
-
-
-
-         return true;}
-   /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top to bottom, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     *and the board is NOT modified.
-     */
-     public boolean checkV (String word, int row, int col){
-       boolean safe = true;
-       for (int i = 0; i < word.length(); row ++){
-       if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-         return false;
-       }
-       else {i ++;}
-     }
-     return safe;}
-
-    public boolean addWordVertical(String word,int row, int col){
-      if(col >= data[0].length || ((row + word.length() - 1) >= data.length)){
-        return false;
-      }
-      if (checkV (word, row, col) == false){
-        return false;
-      }
-
-      else { for (int i = 0; i < word.length(); row ++){
-               data[row][col] = word.charAt(i);
+               r ++;
+             }
+           }
+        if (colIncrement == 0){
+           for (int i = 0; i < word.length();){
+               data[r][c] = word.charAt(i);
                i ++;
-      }}
+               r ++;
+             }
+         }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); col --){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r ++;
+             }
+         }
+       }
+
+  if (rowIncrement == 0){
+         if (colIncrement == 1){
+           for (int i = 0; i < word.length(); col ++){
+               data[r][c] = word.charAt(i);
+               i ++;
+             }
+           }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); col --){
+               data[r][c] = word.charAt(i);
+               i ++;
+             }
+         }
+       }
+
+  if (rowIncrement == -1){
+         if (colIncrement == 1){
+           for (int i = 0; i < word.length(); col ++){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+           }
+         if (colIncrement == 0){
+           for (int i = 0; i < word.length();){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+         }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); col --){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+         }
+       }
+     return true;}
+
+
+
+
+
+
+
+
+
+
+
+
+  public boolean Safe ( int r, int c, String word, int rowIncrement, int colIncrement){
+    if (rowIncrement == 0 && colIncrement == -1){
+          for (int i = 0; i < word.length(); col --){
+            if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+              return false;
+            }
+            else {i ++;}
+          }}
+    if (rowIncrement == 0 && colIncrement == 0){
+        return false;
+        }
+    if (rowIncrement == 0 && colIncrement == 1){
+          for (int i = 0; i < word.length(); col ++){
+            if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+              return false;
+            }
+            else {i ++;}
+          }}
+    if (rowIncrement == 1 && colIncrement == -1){
+              for (int i = 0; i < word.length(); col --){
+                if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                  return false;
+                }
+                else {
+                  i ++;
+                  r ++;}
+              }}
+    if (rowIncrement == 1 && colIncrement == 0){
+                      for (int i = 0; i < word.length();){
+                        if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                          return false;
+                        }
+                        else {
+                          i ++;
+                          r ++;}
+                      }}
+    if (rowIncrement == 1 && colIncrement == 1){
+                      for (int i = 0; i < word.length(); col ++){
+                        if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                          return false;
+                        }
+                        else {
+                              i ++;
+                              r ++;}
+                        }}
+
+    if (rowIncrement == -1 && colIncrement == -1){
+                    for (int i = 0; i < word.length(); col --){
+                      if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
+    if (rowIncrement == -1 && colIncrement == 0){
+                    for (int i = 0; i < word.length();){
+                      if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
+      if (rowIncrement == -1 && colIncrement == 1){
+                    for (int i = 0; i < word.length(); col ++){
+                      if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
       return true;
-    }
-    /**Attempts to add a given word to the specified position of the WordGrid.
-  *The word is added from top left to bottom right, must fit on the WordGrid,
-  *and must have a corresponding letter to match any letters that it overlaps.
-  *
-  *@param word is any text to be added to the word grid.
-  *@param row is the vertical locaiton of where you want the word to start.
-  *@param col is the horizontal location of where you want the word to start.
-  *@return true when the word is added successfully. When the word doesn't fit,
-  *or there are overlapping letters that do not match, then false is returned.
-  */
-  public boolean checkTLBRD(String word,int row, int col){
-    boolean safe = true;
-    for (int i = 0; i < word.length(); i ++){
-    if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-      return false;
-    }
-    else {
-      row ++;
-      col ++;}
-  }
-  return safe;}
-
- public boolean addWordDiagonal(String word,int row, int col){
-   if(col >= data[0].length || ((row + word.length() - 1) >= data.length)){
-     return false;
-   }
-   if(row >= data.length || ((col + word.length() - 1) >= data[0].length)){
-     return false;
-   }
-
-   if (checkTLBRD (word, row, col) == false){
-     return false;
-   }
-
-   else { for (int i = 0; i < word.length();i ++){
-            data[row][col] = word.charAt(i);
-            row ++;
-            col ++;
-   }}
-   return true;
- }}
+                  }
+                }

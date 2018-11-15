ca8881e403f633e6013c4e02afdddecdce0cbec2
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-15 11:17:48 -0500

    Complies with other method

--- WordSearch.java
+++ WordSearch.java
@@ -70,180 +70,180 @@ public class WordSearch{
           data[x][y] = '_';
         }
       }
-    }}
-//
-//     /**Each row is a new line, there is a space between each letter
-//      *@return a String with each character separated by spaces, and rows
-//      *separated by newlines.
-//      */
-//     public String toString(){
-//       String sad = "";
-//       for (int x = 0; x < data.length; x ++){
-//         sad = sad + '|';
-//         for (int y = 0; y < data[x].length; y ++){
-//           sad = sad + data [x][y] + " ";
-//         }
-//         sad = sad + "\n";
-//     }
-//     sad = sad + "Words: ";
-//     return sad;
-//   }
-//
-//
-//     /**Attempts to add a given word to the specified position of the WordGrid.
-//      *The word is added from left to right, must fit on the WordGrid, and must
-//      *have a corresponding letter to match any letters that it overlaps.
-//      *
-//      *@param word is any text to be added to the word grid.
-//      *@param row is the vertical locaiton of where you want the word to start.
-//      *@param col is the horizontal location of where you want the word to start.
-//      *@return true when the word is added successfully. When the word doesn't fit,
-//      * or there are overlapping letters that do not match, then false is returned
-//      * and the board is NOT modified.
-//      */
-// private boolean addWord( int r, int c, String word, int rowIncrement, int colIncrement){
-// if (Safe(r,c,word,rowIncrement,colIncrement) == false){return false;}
-//   if (rowIncrement == 1){
-//         if (colIncrement == 1){
-//            for (int i = 0; i < word.length(); col ++){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r ++;
-//              }
-//            }
-//         if (colIncrement == 0){
-//            for (int i = 0; i < word.length();){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r ++;
-//              }
-//          }
-//          if (colIncrement == -1){
-//            for (int i = 0; i < word.length(); col --){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r ++;
-//              }
-//          }
-//        }
-//
-//   if (rowIncrement == 0){
-//          if (colIncrement == 1){
-//            for (int i = 0; i < word.length(); col ++){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//              }
-//            }
-//          if (colIncrement == -1){
-//            for (int i = 0; i < word.length(); col --){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//              }
-//          }
-//        }
-//
-//   if (rowIncrement == -1){
-//          if (colIncrement == 1){
-//            for (int i = 0; i < word.length(); col ++){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r --;
-//              }
-//            }
-//          if (colIncrement == 0){
-//            for (int i = 0; i < word.length();){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r --;
-//              }
-//          }
-//          if (colIncrement == -1){
-//            for (int i = 0; i < word.length(); col --){
-//                data[r][c] = word.charAt(i);
-//                i ++;
-//                r --;
-//              }
-//          }
-//        }
-//      return true;}
-//
-//   public boolean Safe ( int r, int c, String word, int rowIncrement, int colIncrement){
-//     if (rowIncrement == 0 && colIncrement == -1){
-//           for (int i = 0; i < word.length(); col --){
-//             if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//               return false;
-//             }
-//             else {i ++;}
-//           }}
-//     if (rowIncrement == 0 && colIncrement == 0){
-//         return false;
-//         }
-//     if (rowIncrement == 0 && colIncrement == 1){
-//           for (int i = 0; i < word.length(); col ++){
-//             if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//               return false;
-//             }
-//             else {i ++;}
-//           }}
-//     if (rowIncrement == 1 && colIncrement == -1){
-//               for (int i = 0; i < word.length(); col --){
-//                 if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                   return false;
-//                 }
-//                 else {
-//                   i ++;
-//                   r ++;}
-//               }}
-//     if (rowIncrement == 1 && colIncrement == 0){
-//                       for (int i = 0; i < word.length();){
-//                         if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                           return false;
-//                         }
-//                         else {
-//                           i ++;
-//                           r ++;}
-//                       }}
-//     if (rowIncrement == 1 && colIncrement == 1){
-//                       for (int i = 0; i < word.length(); col ++){
-//                         if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                           return false;
-//                         }
-//                         else {
-//                               i ++;
-//                               r ++;}
-//                         }}
-//
-//     if (rowIncrement == -1 && colIncrement == -1){
-//                     for (int i = 0; i < word.length(); col --){
-//                       if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                         return false;
-//                         }
-//                         else {
-//                               i ++;
-//                               r --;}
-//                             }}
-//     if (rowIncrement == -1 && colIncrement == 0){
-//                     for (int i = 0; i < word.length();){
-//                       if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                         return false;
-//                         }
-//                         else {
-//                               i ++;
-//                               r --;}
-//                             }}
-//       if (rowIncrement == -1 && colIncrement == 1){
-//                     for (int i = 0; i < word.length(); col ++){
-//                       if (data[row][col] != '_' && data[row][col] != word.charAt(i)){
-//                         return false;
-//                         }
-//                         else {
-//                               i ++;
-//                               r --;}
-//                             }}
-//       return true;
-//                   }
-//
-// //Was helped by Moududur Rahman
+    }
+
+    /**Each row is a new line, there is a space between each letter
+     *@return a String with each character separated by spaces, and rows
+     *separated by newlines.
+     */
+    public String toString(){
+      String sad = "";
+      for (int x = 0; x < data.length; x ++){
+        sad = sad + '|';
+        for (int y = 0; y < data[x].length; y ++){
+          sad = sad + data [x][y] + " ";
+        }
+        sad = sad + "\n";
+    }
+    sad = sad + "Words: ";
+    return sad;
+  }
+
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from left to right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     * or there are overlapping letters that do not match, then false is returned
+     * and the board is NOT modified.
+     */
+private boolean addWord( int r, int c, String word, int rowIncrement, int colIncrement){
+if (Safe(r,c,word,rowIncrement,colIncrement) == false){return false;}
+  if (rowIncrement == 1){
+        if (colIncrement == 1){
+           for (int i = 0; i < word.length(); c ++){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r ++;
+             }
+           }
+        if (colIncrement == 0){
+           for (int i = 0; i < word.length();){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r ++;
+             }
+         }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); c --){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r ++;
+             }
+         }
+       }
+
+  if (rowIncrement == 0){
+         if (colIncrement == 1){
+           for (int i = 0; i < word.length(); c ++){
+               data[r][c] = word.charAt(i);
+               i ++;
+             }
+           }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); c --){
+               data[r][c] = word.charAt(i);
+               i ++;
+             }
+         }
+       }
+
+  if (rowIncrement == -1){
+         if (colIncrement == 1){
+           for (int i = 0; i < word.length(); c ++){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+           }
+         if (colIncrement == 0){
+           for (int i = 0; i < word.length();){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+         }
+         if (colIncrement == -1){
+           for (int i = 0; i < word.length(); c --){
+               data[r][c] = word.charAt(i);
+               i ++;
+               r --;
+             }
+         }
+       }
+     return true;}
+
+  public boolean Safe ( int r, int c, String word, int rowIncrement, int colIncrement){
+    if (rowIncrement == 0 && colIncrement == -1){
+          for (int i = 0; i < word.length(); c --){
+            if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+              return false;
+            }
+            else {i ++;}
+          }}
+    if (rowIncrement == 0 && colIncrement == 0){
+        return false;
+        }
+    if (rowIncrement == 0 && colIncrement == 1){
+          for (int i = 0; i < word.length(); c ++){
+            if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+              return false;
+            }
+            else {i ++;}
+          }}
+    if (rowIncrement == 1 && colIncrement == -1){
+              for (int i = 0; i < word.length(); c --){
+                if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                  return false;
+                }
+                else {
+                  i ++;
+                  r ++;}
+              }}
+    if (rowIncrement == 1 && colIncrement == 0){
+                      for (int i = 0; i < word.length();){
+                        if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                          return false;
+                        }
+                        else {
+                          i ++;
+                          r ++;}
+                      }}
+    if (rowIncrement == 1 && colIncrement == 1){
+                      for (int i = 0; i < word.length(); c ++){
+                        if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                          return false;
+                        }
+                        else {
+                              i ++;
+                              r ++;}
+                        }}
+
+    if (rowIncrement == -1 && colIncrement == -1){
+                    for (int i = 0; i < word.length(); c --){
+                      if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
+    if (rowIncrement == -1 && colIncrement == 0){
+                    for (int i = 0; i < word.length();){
+                      if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
+      if (rowIncrement == -1 && colIncrement == 1){
+                    for (int i = 0; i < word.length(); c ++){
+                      if (data[r][c] != '_' && data[r][c] != word.charAt(i)){
+                        return false;
+                        }
+                        else {
+                              i ++;
+                              r --;}
+                            }}
+      return true;
+                  }
+
+//Was helped by Moududur Rahman
 // private void addAllWords() {
 //   int xc;
 //   int yc;
@@ -268,12 +268,12 @@ public class WordSearch{
 //     }
 // }
 // }
-// }
-//
-//
-//
-//
-// //declare new file
-// //declare new scanner that takes file as an input
-// //use scanner methods to add words into the ArrayList
-// // Instantiate ArrayList somewhere
+}
+
+
+
+
+//declare new file
+//declare new scanner that takes file as an input
+//use scanner methods to add words into the ArrayList
+// Instantiate ArrayList somewhere
